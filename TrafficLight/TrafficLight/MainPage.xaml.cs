bbe7d032e8d9af7c0050de0453e17d926c5c5c08
TheKing Surfer <eitanalbo06@gmail.com>
2023-09-21 09:44:21 +0300

    Added Auto button and some color pallet changes.

--- TrafficLight/TrafficLight/MainPage.xaml.cs
+++ TrafficLight/TrafficLight/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +23,18 @@ namespace TrafficLight
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private SolidColorBrush redBrush;
+        private SolidColorBrush yellowBrush;
+        private SolidColorBrush greenBrush;
+        private int currentLightIndex;
         public MainPage()
         {
             this.InitializeComponent();
+            redBrush = new SolidColorBrush(Windows.UI.Colors.DarkRed);
+            yellowBrush = new SolidColorBrush(Windows.UI.Colors.DarkOrange);
+            greenBrush = new SolidColorBrush(Windows.UI.Colors.DarkGreen);
+
+            currentLightIndex = 0; // Start with red light
         }
 
         private void btnManual_Click(object sender, RoutedEventArgs e)
@@ -60,9 +70,44 @@ namespace TrafficLight
                 elpGreen.Fill = new SolidColorBrush(Windows.UI.Colors.Gray);
             }
         }
-        private void btnAuto_Click(object sender, RoutedEventArgs e)
+        private async void btnAuto_Click(object sender, RoutedEventArgs e)
         {
+            await Task.Delay(3000);
+            SetLightColor(1);
 
+            // Change to green after 3 seconds
+            await Task.Delay(3000);
+            SetLightColor(2);
+
+            // Change back to red after 3 seconds
+            await Task.Delay(3000);
+            SetLightColor(0);
         }
+        private void SetLightColor(int lightIndex)
+        {
+            // Reset all lights to gray
+            elpRed.Fill = new SolidColorBrush(Windows.UI.Colors.Gray);
+            elpYellow.Fill = new SolidColorBrush(Windows.UI.Colors.Gray);
+            elpGreen.Fill = new SolidColorBrush(Windows.UI.Colors.Gray);
+
+            // Set the appropriate light to its color
+            switch (lightIndex)
+            {
+                case 0: // Red
+                    elpRed.Fill = redBrush;
+                    break;
+                case 1: // Yellow
+                    elpYellow.Fill = yellowBrush;
+                    break;
+                case 2: // Green
+                    elpGreen.Fill = greenBrush;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // Other event handlers and methods can go here...
     }
 }
+
