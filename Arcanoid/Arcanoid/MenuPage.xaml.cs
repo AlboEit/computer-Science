11e197ccc506d9ce5c847230ffe94b358ef73e39
wave surf <eitanxbox1234@gmail.com>
2023-11-05 11:57:40 +0200

    Added Menu working buttons

--- Arcanoid/Arcanoid/MenuPage.xaml.cs
+++ Arcanoid/Arcanoid/MenuPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -26,5 +27,35 @@ namespace Arcanoid
         {
             this.InitializeComponent();
         }
+        private void btnPointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            // Change the image source when hovering over the button
+            if (sender is Button button)
+            {
+                string imageName = button.Name.Substring(3);
+                Image image = button.Content as Image;
+                if (image != null)
+                {
+                    image.Source = new BitmapImage(new Uri($"ms-appx:///Assets/Buttons/{imageName} (2).png"));
+                }
+            }
+        }
+
+        private void btnPointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            // Restore the original image source when exiting the hover state
+            if (sender is Button button)
+            {
+                string imageName = button.Name.Substring(3);
+                Image image = button.Content as Image;
+                if (image != null)
+                {
+                    image.Source = new BitmapImage(new Uri($"ms-appx:///Assets/Buttons/{imageName} (1).png"));
+                }
+            }
+        }
+
     }
+
+
 }
