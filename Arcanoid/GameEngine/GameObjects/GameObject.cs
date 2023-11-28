f7e368ff7939fcb7844bf2681ffb57693c2e5923
TheKingSurfer <eitanalbo06@gmail.com>
2023-11-28 15:28:55 +0200

    Added some stuff

---
+++ Arcanoid/GameEngine/GameObjects/GameObject.cs
@@ -0,0 +1,66 @@
+﻿using GameEngine.GameServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Arcanoid.GameObjects
+{
+    public abstract class GameObject
+    {
+        protected double _X;
+        protected double _Y;
+
+        protected double _placeX;
+        protected double _placeY;
+
+        public Image Image { get; set; }
+        protected string _filename;
+
+        public double Height => Image.ActualHeight;
+        public double Width => Image.ActualWidth;
+
+        public virtual Rect Rect => new Rect(_X, _Y, Width, Height);
+
+        public bool Collisional { get; set; } = true;// אפשר להתנגש בו , לא שקוף
+        protected Scene _scene;//זירת המשחק
+
+        /// <summary>
+        /// The constractur builds a basic object
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="placeX"></param>
+        /// <param name="placeY"></param>
+        /// <param name="image"></param>
+        /// <param name="filename"></param>
+        /// <param name="collisional"></param>
+        /// <param name="scene"></param>
+        protected GameObject( double placeX, double placeY, Image image, string filename, bool collisional, Scene scene)
+        {
+            _X = placeX;
+            _Y = placeY;
+            _placeX = placeX;
+            _placeY = placeY;
+            Image = image;
+            _filename = filename;
+            _scene = scene;
+        }
+
+        public virtual void collide(GameObject gameObject) 
+        {
+            Canvas.SetLeft(Image, _X);
+            Canvas.SetTop(Image, _Y);
+            
+        }
+        protected void SetImage(string filename)
+        {
+            Image.Source = new BitmapImage(new Uri($"ms-appx:///Assets/{_filename}"));
+        }
+
+    }
+}
---
+++ Arcanoid/GameEngine/GameObjects/MoveObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arcanoid.GameObjects;
+using GameEngine;
+using GameEngine.GameServices;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace GameEngine.GameObjects
+{
+    public abstract class MoveObject 
+    {
+        
+    }
+}
