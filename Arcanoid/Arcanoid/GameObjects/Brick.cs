ff7bcb82ccaae2b9c5b975a0eae72a5e4acadddb
TheKingSurfer <eitanalbo06@gmail.com>
2023-11-30 14:17:47 +0200

    Added brick class

--- Arcanoid/Arcanoid/GameObjects/Brick.cs
+++ Arcanoid/Arcanoid/GameObjects/Brick.cs
@@ -4,10 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GameEngine;
+using GameEngine.GameServices;
+using Windows.UI.Xaml.Controls;
 
 namespace Arcanoid.GameObjects
 {
-    public class Brick 
+    public class Brick:GameObject
     {
+        public enum BrickType
+        {
+            Green = 0,Pink = 1,Yellow = 2
+        }
+        private BrickType _brickType;
+
+        public Brick(BrickType brickType, double placeX, double placeY, Image image, string filename, bool collisional, Scene scene) : base(placeX, placeY, image, filename, collisional, scene)
+        {
+            Image.Width = Width;
+            Image.Height = Height;
+            _brickType = brickType;
+            SetImage();
+        }
+
+        private void SetImage()
+        {
+            switch (_brickType)
+            {
+                case BrickType.Green:
+                    base.SetImage("Assets/Brick/Brick_green.jpg");
+                    break;
+                case BrickType.Pink:
+                    base.SetImage("Assets/Brick/Brick_Pink.jpg");
+                    break;
+                case BrickType.Yellow:
+                    base.SetImage("Assets/Brick/Brick_yellow.jpg");
+                    break;
+            }
+        }
     }
 }
